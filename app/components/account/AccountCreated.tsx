268fb812745894f97a3cd29f4ed6408401b98ce5
jsbowen79 <jsbowen79@outlook.com>
2026-09-18 12:54:27 -0600

    registration form functioning and saving to database.

--- app/components/account/AccountCreated.tsx
+++ app/components/account/AccountCreated.tsx
@@ -9,10 +9,10 @@ export default function AccountCreated({ account }: AccountCreatedProps) {
     <section>
       <h3>Registration Success</h3>
       <p>Your account has been created.</p>
-      <p>`Account Id: ${account.id}`</p>
-      <p>`Name: ${account.name}`</p>
-      <p>`Email: ${account.email}`</p>
-      <p>`Created: ${account.createdAt}`</p>
+      <p>Account Id: {account.id}</p>
+      <p>Name: {account.name}</p>
+      <p>Email: {account.email}</p>
+      <p>Account Created: {account.createdAt}</p>
 
       <h3>We hope you enjoy our App!</h3>
     </section>
